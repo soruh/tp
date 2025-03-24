65d7afe8abe48781dbc8a9933d330da2e77558bf
soruh <mail@soruh.de>
2025-03-24 15:01:27 +0100

    executeCrazyCatch

--- src/d/actor/d_a_cow.cpp
+++ src/d/actor/d_a_cow.cpp
@@ -2139,56 +2139,56 @@ void daCow_c::executeCrazyCatch() {
     f32 fVar2 = -220.0f;
     int bVar1 = field_0xc60;
 
-    if (bVar1 != 2) {
-        if (bVar1 < 2) {
-            mShouldSetEffect = 1;
-            if (!field_0xc60 && mpMorf->isStop()) {
-                setBck(5, 2, 0.0f, 1.0f);
-                field_0xc60 = 1;
-            }
-
-            if (daPy_getPlayerActorClass()->speedF == 0.0f) {
-                setBck(0x10, 0, 0.0f, 1.0f);
-                field_0xc60 = 2;
-            }
-        } else if (bVar1 < 5) {
-            fVar2 = -260.0f;
-
-            if (!field_0xc90) {
-                if (field_0xc60 == 3) {
-                    setBck(0x12, 2, 0.0f, 1.0f);
-                    field_0xc90 = 0x3c;
-                    field_0xc60 = 4;
-                } else {
-                    setBck(0x11, 2, 0.0f, 1.0f);
-                    field_0xc90 = 0x3c;
-                    field_0xc60 = 3;
-                }
-            }
+    switch (field_0xc60) {
+    case 0:
+    case 1:
+        mShouldSetEffect = 1;
+        if (!field_0xc60 && mpMorf->isStop()) {
+            setBck(5, 2, 0.0f, 1.0f);
+            field_0xc60 = 1;
         }
 
-    } else {
+        if (!daPy_getPlayerActorClass()->speedF) {
+            setBck(0x10, 0, 0.0f, 1.0f);
+            field_0xc60 = 2;
+        }
+        break;
+    case 2:
         fVar2 = -260.0f;
         if (mpMorf->isStop()) {
             setBck(0x12, 2, 0.0f, 1.0f);
+            field_0xc90 = 0x3c;
             field_0xc60 = 4;
         }
+        break;
+    case 3:
+    case 4:
+        fVar2 = -260.0f;
+
+        if (!field_0xc90) {
+            if (field_0xc60 == 3) {
+                setBck(0x12, 2, 0.0f, 1.0f);
+                field_0xc90 = 0x3c;
+                field_0xc60 = 4;
+            } else {
+                setBck(0x11, 2, 0.0f, 1.0f);
+                field_0xc90 = 0x3c;
+                field_0xc60 = 3;
+            }
+        }
+        break;
     }
 
-    if (mFlags != 0) {
+    if (mFlags) {
         if (mFlags & 8) {
             initCrazyThrow(0);
-        } else {
-            if (mFlags & 0x10) {
-                initCrazyThrow(1);
+        } else if (mFlags & 0x10) {
+            initCrazyThrow(1);
+        } else if (mFlags & 4) {
+            if (!daPy_getPlayerActorClass()->speedF) {
+                initCrazyAttack(0);
             } else {
-                if (mFlags & 4) {
-                    if (daPy_getPlayerActorClass()->speedF == 0.0f) {
-                        initCrazyAttack(0);
-                    } else {
-                        initCrazyAttack(1);
-                    }
-                }
+                initCrazyAttack(1);
             }
         }
         mFlags = 0;
